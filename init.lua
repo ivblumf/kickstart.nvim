7d9962ab1056143b2a6b5edab2693c564bf264b6
Frank Blum <frank.blum@ivz.de>
2024-12-30 08:37:38 +0100

    Add CopilteChat and init.lua suggestion

--- init.lua
+++ init.lua
@@ -475,6 +475,19 @@ require('lazy').setup({
   --     end, 100)
   --   end,
   -- },
+  --
+  {
+    'CopilotC-Nvim/CopilotChat.nvim',
+    dependencies = {
+      { 'github/copilot.vim' }, -- or zbirenbaum/copilot.lua
+      { 'nvim-lua/plenary.nvim', branch = 'master' }, -- for curl, log and async functions
+    },
+    build = 'make tiktoken', -- Only on MacOS or Linux
+    opts = {
+      -- See Configuration section for options
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
 
   {
     'karb94/neoscroll.nvim',
